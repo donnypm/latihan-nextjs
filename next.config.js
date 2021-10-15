a09181850b055dabbb105cab478e7020b8c28880
donnypm <donnypramudia04@gmail.com>
2021-10-15 17:30:53 +0700

    latihan-nextjs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-};
-
-module.exports = {
-  reactStrictMode: true,
   images: {
     domains: ["image-cdn.medkomtek.com"],
     hostname: "image-cdn.medkomtek.com",
